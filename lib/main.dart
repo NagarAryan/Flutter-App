ec0598f427ed13128d8d6339a82decec6b651ba0
Aryan <aryannagar2002@gmail.com>
2022-02-17 00:34:34 +0530

    Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point foe all apps
+//Center widget is used to center the text here Hello World
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TEST APP'),
+          backgroundColor: Colors.,
+        ),
+      ),
+    ),
+  );
+}
